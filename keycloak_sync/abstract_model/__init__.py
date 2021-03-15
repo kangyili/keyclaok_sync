38abd898a2c84b1a720f9382a9401cd44db71766
Kangyi LI <kangyi.li@polynom.com>
2021-03-15 07:27:38 +0100

    delete useless files

--- keycloak_sync/abstract_model/__init__.py
+++
@@ -1,8 +0,0 @@
-from keycloak_sync.abstract_model.user import User
-from keycloak_sync.abstract_model.loader import Loader
-from keycloak_sync.abstract_model.storageprovider import StorageProvider
-__all__ = [
-    "User",
-    "Loader",
-    "StorageProvider"
-]
--- keycloak_sync/abstract_model/loader.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Loader(ABC):
-    """ basic loader"""
-
-    def __init__(self, format):
-        self.format = format
